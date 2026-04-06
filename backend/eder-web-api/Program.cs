698a4311e8dc3287f54fb344b12f39cfa048ed8c
JohnHacker1998 <yohannes.kindu@gmail.com>
2026-04-06 08:54:54 +0300

    chore: added swagger, reinitialized project, removed unnecessary endpoints

---
+++ backend/eder-web-api/Program.cs
@@ -0,0 +1,22 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.UseSwaggerUI(FileOptions =>
+    {
+        FileOptions.SwaggerEndpoint("/openapi/v1.json","API v1");
+    });
+}
+
+app.UseHttpsRedirection();
+
+app.Run();
+
